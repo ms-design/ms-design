1a36e9ca55bb950e7f024b2f0c2816c79f009834
pyyzcwg2833 <pyyzcwg2833@outlook.com>
2018-12-21 15:05:06 +0800

    ci: update configuration

---
+++ scripts/travis/after-success.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ ${TRAVIS_BRANCH} == 'master' ]];
+then
+  yarn run codecov --token=${CODECOV_TOKEN}
+  cd dist
+  git init
+  git config user.name 'pyyzcwg2833'
+  git config user.email 'pyyzcwg2833@outlook.com'
+  git add -A
+  git commit -m "update documents by travis-ci with build ${TRAVIS_BUILD_NUMBER}"
+  git push --force "https://${GH_TOKEN}@${GH_REF}" master:master
+fi
